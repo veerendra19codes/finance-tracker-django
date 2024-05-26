c2240e873edfc2736050177d44547a8269afb737
veerendra19codes <veerendragumate@gmail.com>
2024-05-26 17:22:24 +0530

    deploy3

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+source /path/to/virtualenv/bin/activate
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+
+
+// "src": "build_files.sh",
+// "use": "@vercel/static-build",
+// "config": {
+//     "distDir": "staticfiles_build"
+// }
